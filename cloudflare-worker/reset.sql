267310d025c4844bc439f3df873e3952bb339541
fllowzle <2239290302@qq.com>
2026-06-24 09:06:20 +0800

    Remove old backup files, fix page titles

---
+++ cloudflare-worker/reset.sql
@@ -0,0 +1,2 @@
+DELETE FROM bookings WHERE email='test@test.com';
+UPDATE rooms SET available = 8 WHERE type='(a) Deluxe Suite' AND date_key='Dec 18';
---
+++ cloudflare-worker/reset2.sql
@@ -0,0 +1,2 @@
+DELETE FROM bookings WHERE name='TestDec17';
+UPDATE rooms SET available = 72 WHERE type='(d) Standard Twin' AND date_key='Dec 17';
